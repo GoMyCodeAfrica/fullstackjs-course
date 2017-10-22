3be8167d026664b618c2d527b39233b357c1e637
morfioce <moncef.bouallagui@gmail.com>
2017-10-22 22:50:27 +0100

    added more exercises

--- 1_8_javascript_exercises/main.js
+++ 1_8_javascript_exercises/main.js
@@ -221,3 +221,63 @@ test(mix_up('mix', 'pod'), 'pox mid');
 test(mix_up('dog', 'dinner'), 'dig donner');
 test(mix_up('gnash', 'sport'), 'spash gnort');
 test(mix_up('pezzy', 'firm'), 'fizzy perm');
+print(separator)
+
+// Ex 10
+/*
+	Given a string, if its length is at least 3,
+	add 'ing' to its end.
+	Unless it already ends in 'ing', in which case
+	add 'ly' instead.
+	If the string length is less than 3, leave it unchanged.
+	Return the resulting string.
+*/
+function verbing(str) {
+	return null;
+}
+
+print("verbing");
+test(verbing('hail'), 'hailing');
+test(verbing('swiming'), 'swimingly');
+test(verbing('do'), 'do');
+print(separator);
+
+// Ex 11
+/*
+	Given a string, find the first appearance of the
+	substring 'not' and 'bad'. If the 'bad' follows
+	the 'not', replace the whole 'not'...'bad' substring
+	with 'good'.
+	Return the resulting string.
+	So 'This dinner is not that bad!' yields:
+	This dinner is good!
+*/
+function not_bad(str) {
+	return null;
+}
+
+print('not_bad')
+test(not_bad('This movie is not so bad'), 'This movie is good');
+test(not_bad('This dinner is not that bad!'), 'This dinner is good!');
+test(not_bad('This tea is not hot'), 'This tea is not hot');
+test(not_bad("It's bad yet not"), "It's bad yet not");
+print(separator);
+
+// Ex 12
+/*
+	Consider dividing a string into two halves.
+	If the length is even, the front and back halves are the same length.
+	If the length is odd, we'll say that the extra char goes in the front half.
+	e.g. 'abcde', the front half is 'abc', the back half 'de'.
+	Given 2 strings, a and b, return a string of the form
+	 a-front + b-front + a-back + b-back
+*/
+function front_back(str1, str2) {
+	return null;
+}
+
+print('front_back')
+test(front_back('abcd', 'xy'), 'abxcdy');
+test(front_back('abcde', 'xyz'), 'abcxydez');
+test(front_back('Kitten', 'Donut'), 'KitDontenut');
+print(separator);
